724bc7c7391862a331d976de2244b78876aa806c
bogusz <bogusz2005@gmail.com>
2023-09-09 19:31:57 +0200

    jo

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,10 @@
+import java.math.BigInteger
+
+fun main(args: Array<String>) {
+
+
+}
+fun BIPow(index: Int): BigInteger = BigInteger.valueOf(2).pow(index)
+fun BI(number: Long): BigInteger = BigInteger.valueOf(number)
+
+
